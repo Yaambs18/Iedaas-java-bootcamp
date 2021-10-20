76d73fff6b32195a4e0d6bab29a44591b7f49e41
yaambs <yanshbhardwaj14@gmail.com>
2021-10-20 13:40:55 +0530

    Data types hands-on added

---
+++ src/com/company/DataTypes.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte age = 21;
+        int subscribeCount = 123456789;
+        long viewCount = 9876543210L;
+        float percentage = 82.4f;
+        char firstLetter = 'A';
+        boolean isAdult = true;
+        System.out.println("Hello World!" + " " +age + " " +subscribeCount + " " +viewCount + " " +percentage + " " +firstLetter + " " +isAdult);
+    }
+}
