99959c36b222febf973586e9a1fce77d4c88f731
yaambs <yanshbhardwaj14@gmail.com>
2021-10-21 17:05:54 +0530

    Constructor overloading hands-on code added

---
+++ src/com/JavaBootCamp/TestConstructor/ConstructorOverloading.java
@@ -0,0 +1,19 @@
+package com.JavaBootCamp.TestConstructor;
+
+public class ConstructorOverloading {
+    int instanceVariable;
+    String type;
+    ConstructorOverloading(int value, String s){
+        instanceVariable = value;
+        type = s;
+    }
+    public static void main(String[] args) {
+        ConstructorOverloading normalCalculator = new ConstructorOverloading(10, "normal");
+        ConstructorOverloading scientificCalculator = new ConstructorOverloading(20, "scientific");
+
+        System.out.println("Instance variable of normal calculator "+normalCalculator.instanceVariable);
+        System.out.println("Instance variable of scientific calculator "+scientificCalculator.type);
+
+
+    }
+}
