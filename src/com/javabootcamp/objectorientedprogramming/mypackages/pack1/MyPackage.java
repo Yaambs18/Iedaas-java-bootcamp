7100bd14851ba5f86fe819e8d9de86eeb81964ec
yaambs <yanshbhardwaj14@gmail.com>
2021-10-22 18:26:07 +0530

    hands-on packages code added successfully

---
+++ src/com/javabootcamp/objectorientedprogramming/mypackages/pack1/MyPackage.java
@@ -0,0 +1,5 @@
+package com.javabootcamp.objectorientedprogramming.mypackages.pack1;
+
+public class MyPackage {
+    public void msg(){System.out.println("I am demo package imported successfully.");}
+}
---
+++ src/com/javabootcamp/objectorientedprogramming/mypackages/pack2/TestPackage.java
@@ -0,0 +1,10 @@
+package com.javabootcamp.objectorientedprogramming.mypackages.pack2;
+import com.javabootcamp.objectorientedprogramming.mypackages.pack1.MyPackage;
+import com.javabootcamp.objectorientedprogramming.mypackages.pack1.*;
+public class TestPackage extends MyPackage {
+    public static void main(String[] args) {
+        MyPackage obj = new MyPackage();
+        obj.msg();
+    }
+
+}
