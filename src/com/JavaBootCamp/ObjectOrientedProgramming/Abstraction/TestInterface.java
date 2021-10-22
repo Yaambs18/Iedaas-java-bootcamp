df67b2c86b7151d505d5373c3d3817d22ff4716a
yaambs <yanshbhardwaj14@gmail.com>
2021-10-22 17:13:40 +0530

    implements the concepts of interfaces

---
+++ src/com/JavaBootCamp/ObjectOrientedProgramming/Abstraction/TestInterface.java
@@ -0,0 +1,22 @@
+package com.JavaBootCamp.ObjectOrientedProgramming.Abstraction;
+
+interface Money{
+    void Operation();
+}
+class Debit implements Money{
+    public void Operation(){
+        System.out.println("Debiting money.");
+    }
+}
+class Credit implements  Money{
+    public void Operation(){
+        System.out.println("Amount crediting");
+    }
+}
+
+public class TestInterface {
+    public static void main(String[] args) {
+        Money d = new Debit();
+        d.Operation();
+    }
+}
