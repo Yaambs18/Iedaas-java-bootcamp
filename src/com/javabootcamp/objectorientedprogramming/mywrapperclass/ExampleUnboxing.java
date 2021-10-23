20060a38c7c4df1e42ab674eed758269fa2a0c6b
yaambs <yanshbhardwaj14@gmail.com>
2021-10-23 11:55:10 +0530

    hands-on Wrapper class unboxing code added successfully

---
+++ src/com/javabootcamp/objectorientedprogramming/mywrapperclass/ExampleUnboxing.java
@@ -0,0 +1,10 @@
+package com.javabootcamp.objectorientedprogramming.mywrapperclass;
+
+public class ExampleUnboxing {
+    public static void main(String[] args) {
+        Integer a=new Integer(3);
+        int j=a;//unboxing
+
+        System.out.println(a+" "+" "+j);
+    }
+}
