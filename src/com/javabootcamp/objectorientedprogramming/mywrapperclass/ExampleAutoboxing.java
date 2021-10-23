1b2259ea56916ea8204f288220844fb89f51fad0
yaambs <yanshbhardwaj14@gmail.com>
2021-10-23 11:54:54 +0530

    hands-on Wrapper class autoboxing code added successfully

---
+++ src/com/javabootcamp/objectorientedprogramming/mywrapperclass/ExampleAutoboxing.java
@@ -0,0 +1,10 @@
+package com.javabootcamp.objectorientedprogramming.mywrapperclass;
+
+public class ExampleAutoboxing {
+    public static void main(String[] args) {
+        int i=20;
+        Integer a = i; //autoboxing
+
+        System.out.println(i+" "+a);
+    }
+}
