3b381cf27a13ce1ba681f97690c507946ff283f8
yaambs <yanshbhardwaj14@gmail.com>
2021-10-25 16:16:27 +0530

    custom thread example code hands-on added

---
+++ src/com/javabootcamp/multithreadingexamples/MyThread.java
@@ -0,0 +1,13 @@
+package com.javabootcamp.multithreadingexamples;
+
+public class MyThread
+{
+    public static void main(String[] args)
+    {
+        Thread t= new Thread("My first thread");
+
+        t.start();
+        String name = t.getName();
+        System.out.println(name);
+    }
+}
