b19962742bf611318ac7d4dc39cf7d61f4a067b8
yaambs <yanshbhardwaj14@gmail.com>
2021-10-26 13:28:07 +0530

    tambola board file creation code addded

--- src/com/javabootcamp/assignment1/TambolaGameAssignment.java
+++ src/com/javabootcamp/assignment1/TambolaGameAssignment.java
@@ -1,7 +1,10 @@
 package com.javabootcamp.assignment1;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
 
 public class TambolaGameAssignment extends Thread{
 
@@ -28,10 +31,11 @@ public class TambolaGameAssignment extends Thread{
             System.out.print(key+":"+ticket.get(key)+"    ");
             row++;
         }
-        System.out.println();
+        System.out.println("\n");
     }
 
     public void displayboard(HashMap<Integer, Boolean> board){
+        System.out.println("Tambola board................");
         Iterator<Integer> itemskey = board.keySet().iterator();
         int row = 0;
         while(itemskey.hasNext())
@@ -43,7 +47,7 @@ public class TambolaGameAssignment extends Thread{
             System.out.print(key+":"+board.get(key)+"    ");
             row++;
         }
-        System.out.println();
+        System.out.println("\n");
     }
 
     public HashMap<Integer, Boolean> boardmaker() {
@@ -66,26 +70,53 @@ public class TambolaGameAssignment extends Thread{
                 System.out.println(random_number);
                 if(playerticket.containsKey(random_number)){
                     playerticket.replace(random_number, true);
-                    displayticket(playerticket);
                 }
+                displayticket(playerticket);
             }
             try {
-                TambolaGameAssignment.sleep(1000);
+                TambolaGameAssignment.sleep(4000);
             }
             catch (Exception e){
                 System.out.println(e);
             }
 
             }
-        System.out.println("Hey! It's Housie, please verify....");
-        checker(playerticket, tambola_board);
+        FileWriter file;
+        String filepath = "/home/yaambs18/IdeaProjects/Iedaas-java-bootcamp/src/com/javabootcamp/assignment1/TambolaBoard.txt";
+        try {
+            file = new FileWriter(filepath);
+            String boarddata = tambola_board.toString();
+            file.write(boarddata);
+            file.close();
         }
-    void checker(HashMap<Integer,Boolean> playerticket, HashMap<Integer,Boolean> tambola_board){
+        catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println("Hey! It's Housie, please verify....");
+        checker(playerticket, filepath);
+        }
+    void checker(HashMap<Integer,Boolean> playerticket, String path){
         System.out.println("Wait I am checking.....");
+        HashMap<Integer, Boolean> board = new HashMap<Integer, Boolean>();
+        try {
+            File boardfile = new File(path);
+            Scanner myReader = new Scanner(boardfile);
+            String data = myReader.nextLine();
+            String tamboladata = data.substring(1,data.length()-1);
+            String[] pairs = tamboladata.split(",");
+            for (int i=0;i<pairs.length;i++) {
+                String pair = pairs[i];
+                String[] keyValue = pair.split("=");
+                board.put(Integer.parseInt(keyValue[0].trim()), Boolean.valueOf(keyValue[1]));
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e);
+        }
         Iterator<Integer> ticketkeys = playerticket.keySet().iterator();
         while(ticketkeys.hasNext()){
             int key=ticketkeys.next();
-            if(!tambola_board.get(key)){
+            if(!board.get(key)){
                 System.out.println("Sorry you did some incorrect removal.");
             }
         }
@@ -96,10 +127,13 @@ public class TambolaGameAssignment extends Thread{
         TambolaGameAssignment obj = new TambolaGameAssignment();
         HashMap<Integer, Boolean> player1 = obj.ticketgenerator();
         obj.displayticket(player1);
+        try {
+            TambolaGameAssignment.sleep(3000);
+        }catch (Exception exp){
+            System.out.println(exp);
+        }
         HashMap<Integer,Boolean> tambolaboard = obj.boardmaker();
         obj.displayboard(tambolaboard);
         obj.randomnumber(player1, tambolaboard);
     }
 }
-
-
